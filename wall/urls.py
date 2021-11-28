c7c959a715e8621ea40e1874b90804d65c2c843f
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2021-11-28 01:18:32 -0500

    3rd commit

--- wall/urls.py
+++ wall/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path     
 from . import views
+
 urlpatterns = [
-    path('wall/', views.index, name='wall'),
-    path('post-message/', views.post_message),
+    path('wall/', views.index),
+    path('wall/post-message/', views.post_message),
     path('wall/comment/', views.comment),
     path('wall/logout/', views.logout)
 ]
